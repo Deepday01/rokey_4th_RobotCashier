ceb0972dbe1cc230d47c3fae3ddef7f715fbc9de
Deepday01 <mhi1248@naver.com>
2026-03-09 14:27:45 +0900

    dev 생성

--- src/cashier_workflow/launch/demo_split.launch.py
+++ src/cashier_workflow/launch/demo_split.launch.py
@@ -10,24 +10,24 @@ def generate_launch_description():
             name="demo_voice_node",
             output="screen",
         ),
-        # Node(
-        #     package="cashier_workflow",
-        #     executable="demo_vision_node",
-        #     name="demo_vision_node",
-        #     output="screen",
-        # ),
+        Node(
+            package="cashier_workflow",
+            executable="demo_vision_node",
+            name="demo_vision_node",
+            output="screen",
+        ),
         Node(
             package="cashier_workflow",
             executable="demo_plan_packing_node",
             name="demo_plan_packing_node",
             output="screen",
         ),
-        Node(
-            package="cashier_workflow",
-            executable="demo_execute_packing_node",
-            name="demo_execute_packing_node",
-            output="screen",
-        ),
+        # Node(
+        #     package="cashier_workflow",
+        #     executable="demo_execute_packing_node",
+        #     name="demo_execute_packing_node",
+        #     output="screen",
+        # ),
         Node(
             package="cashier_workflow",
             executable="workflow_node",
